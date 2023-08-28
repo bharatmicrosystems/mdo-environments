8e353a54e3442d43bb75b7dc87c8ae58f17462ff
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-08-28 14:32:10 +0000

    Added external secrsts

--- terraform/app.tf
+++ terraform/app.tf
@@ -10,3 +10,16 @@ resource "kubectl_manifest" "apps" {
   yaml_body = each.value
   override_namespace = "argocd"
 }
+
+data "kubectl_file_documents" "external-secrets" {
+    content = file("../manifests/argocd/external-secrets.yaml")
+}
+
+resource "kubectl_manifest" "external-secrets" {
+  depends_on = [
+    kubectl_manifest.argocd,
+  ]
+  for_each  = data.kubectl_file_documents.external-secrets.manifests
+  yaml_body = each.value
+  override_namespace = "argocd"
+}
