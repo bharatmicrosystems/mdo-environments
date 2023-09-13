7f7f61ebcb1e6c180cadfa807e854a1c1d1edb36
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-09-13 11:58:59 +0000

    Created CD rule

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -7,8 +7,13 @@ resource "google_container_cluster" "main" {
   name               = "${var.cluster_name}-${var.branch}"
   location           = var.location
   initial_node_count = 3
-  binary_authorization {
-    evaluation_mode = "PROJECT_SINGLETON_POLICY_ENFORCE"
+  dynamic "binary_authorization" {
+    for_each = var.branch == "prod" ? [1] : []
+    content {
+      binary_authorization {
+        evaluation_mode = "PROJECT_SINGLETON_POLICY_ENFORCE"
+      }
+    }
   }
   node_config {
     service_account = google_service_account.main.email
