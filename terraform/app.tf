4ab41428ce7893c2df2d4494110209d072eb298b
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-09-16 14:25:22 +0000

    Configure External Secrets

--- terraform/app.tf
+++ terraform/app.tf
@@ -23,3 +23,15 @@ resource "kubectl_manifest" "external-secrets" {
   yaml_body = each.value
   override_namespace = "argocd"
 }
+
+data "kubectl_file_documents" "gcpsm-secret" {
+    content = file("../manifests/argocd/gcpsm-secret.yaml")
+}
+
+resource "kubectl_manifest" "gcpsm-secrets" {
+  depends_on = [
+    kubectl_manifest.external-secrets,
+  ]
+  for_each  = data.kubectl_file_documents.gcpsm-secret.manifests
+  yaml_body = each.value
+}
