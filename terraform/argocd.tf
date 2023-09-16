6f4c1b24566621966c42a80340af53f52cdcd45c
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2023-09-16 09:00:17 +0000

    Baselined

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -12,25 +12,25 @@ module "gke_auth" {
   use_private_endpoint = false
 }
 
-#data "kubectl_file_documents" "namespace" {
-#  content = file("../manifests/argocd/namespace.yaml")
-#} 
+data "kubectl_file_documents" "namespace" {
+  content = file("../manifests/argocd/namespace.yaml")
+}
 
-#data "kubectl_file_documents" "argocd" {
-#  content = file("../manifests/argocd/install.yaml")
-#}
+data "kubectl_file_documents" "argocd" {
+  content = file("../manifests/argocd/install.yaml")
+}
 
-#resource "kubectl_manifest" "namespace" {
-#  for_each  = data.kubectl_file_documents.namespace.manifests
-#  yaml_body = each.value 
-#  override_namespace = "argocd"
-#}
+resource "kubectl_manifest" "namespace" {
+  for_each  = data.kubectl_file_documents.namespace.manifests
+  yaml_body = each.value 
+  override_namespace = "argocd"
+}
 
-#resource "kubectl_manifest" "argocd" {
-#  depends_on = [
-#    kubectl_manifest.namespace,
-#  ]
-#  for_each  = data.kubectl_file_documents.argocd.manifests
-#  yaml_body = each.value
-#  override_namespace = "argocd"
-#}
+resource "kubectl_manifest" "argocd" {
+  depends_on = [
+    kubectl_manifest.namespace,
+  ]
+  for_each  = data.kubectl_file_documents.argocd.manifests
+  yaml_body = each.value
+  override_namespace = "argocd"
+}
